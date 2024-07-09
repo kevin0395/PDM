79c10b865847ba41f32da6480451a9ce7083daf7
kevin0395 <kevincorazza09@gmail.com>
2024-07-09 22:58:45 +0200

    first commit

--- main.js
+++ main.js
@@ -7,42 +7,38 @@ async function createPreview(container, fileUrl) {
   components.camera = new OBC.SimpleCamera(components);
   components.raycaster = new OBC.SimpleRaycaster(components);
 
-  try {
-    components.init();
+  components.init();
 
-    const grid = new OBC.SimpleGrid(components);
-    components.scene.setup();
-    const scene = components.scene.get();
+  const grid = new OBC.SimpleGrid(components);
+  components.scene.setup();
+  const scene = components.scene.get();
 
-    let fragmentIfcLoader = new OBC.FragmentIfcLoader(components);
-    await fragmentIfcLoader.setup();
-    fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = false;
-    fragmentIfcLoader.settings.webIfc.OPTIMIZE_PROFILES = true;
+  let fragmentIfcLoader = new OBC.FragmentIfcLoader(components);
+  await fragmentIfcLoader.setup();
+  fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = false;
+  fragmentIfcLoader.settings.webIfc.OPTIMIZE_PROFILES = true;
 
+  async function loadIfcAsFragments() {
     const file = await fetch(fileUrl);
-    if (!file.ok) {
-      throw new Error(`Erreur de chargement du fichier IFC : ${fileUrl}`);
-    }
     const data = await file.arrayBuffer();
     const buffer = new Uint8Array(data);
     const model = await fragmentIfcLoader.load(buffer, "example");
     scene.add(model);
-  } catch (error) {
-    console.error("Erreur lors du chargement de l'IFC :", error);
   }
+
+  await loadIfcAsFragments();
 }
 
-const preview = document.querySelector('.preview');
-if (preview) {
+document.querySelectorAll('.preview').forEach(async (preview) => {
   const fileUrl = `./model/${preview.getAttribute('data-ifc')}`;
   const container = document.createElement('div');
   container.style.width = '100%';
   container.style.height = '100%';
   preview.appendChild(container);
 
-  createPreview(container, fileUrl);
+  await createPreview(container, fileUrl);
 
   preview.addEventListener('click', () => {
     window.location.href = `viewer.html?file=${preview.getAttribute('data-ifc')}`;
   });
-}
+});
